29929d066f02efee377d872d19d38dbb528829e1
Диана <diana@MacBook-Pro-Diana.local>
2019-12-15 18:14:18 +0300

    Small fixes

--- Inscription/Form1.Designer.cs
+++ Inscription/Form1.Designer.cs
@@ -32,73 +32,61 @@
             this.devicesCombo = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.videoSourcePlayer = new AForge.Controls.VideoSourcePlayer();
-            this.labelSaved = new System.Windows.Forms.Label();
             this.takePictureBtn = new System.Windows.Forms.Button();
             this.buttonStart = new System.Windows.Forms.Button();
             this.buttonStop = new System.Windows.Forms.Button();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer = new System.Windows.Forms.Timer(this.components);
             this.labelTicks = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.buttonVideo = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.SuspendLayout();
             // 
             // devicesCombo
             // 
             this.devicesCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.devicesCombo.FormattingEnabled = true;
-            this.devicesCombo.Location = new System.Drawing.Point(18, 48);
-            this.devicesCombo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.devicesCombo.Location = new System.Drawing.Point(12, 34);
             this.devicesCombo.Name = "devicesCombo";
-            this.devicesCombo.Size = new System.Drawing.Size(372, 28);
+            this.devicesCombo.Size = new System.Drawing.Size(328, 21);
             this.devicesCombo.TabIndex = 0;
             this.devicesCombo.SelectedIndexChanged += new System.EventHandler(this.devicesCombo_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(18, 23);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(12, 18);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(127, 20);
+            this.label1.Size = new System.Drawing.Size(97, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Choose Source :";
+            this.label1.Text = "Источник кадров:";
             // 
             // videoSourcePlayer
             // 
-            this.videoSourcePlayer.Location = new System.Drawing.Point(18, 91);
-            this.videoSourcePlayer.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.videoSourcePlayer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.videoSourcePlayer.Location = new System.Drawing.Point(13, 151);
             this.videoSourcePlayer.Name = "videoSourcePlayer";
-            this.videoSourcePlayer.Size = new System.Drawing.Size(374, 331);
+            this.videoSourcePlayer.Size = new System.Drawing.Size(325, 281);
             this.videoSourcePlayer.TabIndex = 2;
             this.videoSourcePlayer.Text = "videoSourcePlayer1";
             this.videoSourcePlayer.VideoSource = null;
             // 
-            // labelSaved
-            // 
-            this.labelSaved.AutoSize = true;
-            this.labelSaved.Location = new System.Drawing.Point(13, 517);
-            this.labelSaved.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.labelSaved.Name = "labelSaved";
-            this.labelSaved.Size = new System.Drawing.Size(127, 20);
-            this.labelSaved.TabIndex = 3;
-            this.labelSaved.Text = "Capture Saved : ";
-            // 
             // takePictureBtn
             // 
-            this.takePictureBtn.Location = new System.Drawing.Point(21, 551);
-            this.takePictureBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.takePictureBtn.Location = new System.Drawing.Point(15, 447);
             this.takePictureBtn.Name = "takePictureBtn";
-            this.takePictureBtn.Size = new System.Drawing.Size(369, 35);
+            this.takePictureBtn.Size = new System.Drawing.Size(320, 28);
             this.takePictureBtn.TabIndex = 4;
-            this.takePictureBtn.Text = "Take a #Selfie :)";
+            this.takePictureBtn.Text = "Save current frame";
             this.takePictureBtn.UseVisualStyleBackColor = true;
             this.takePictureBtn.Click += new System.EventHandler(this.takePictureBtn_Click);
             // 
             // buttonStart
             // 
-            this.buttonStart.Location = new System.Drawing.Point(18, 460);
+            this.buttonStart.Location = new System.Drawing.Point(10, 86);
+            this.buttonStart.Margin = new System.Windows.Forms.Padding(2);
             this.buttonStart.Name = "buttonStart";
-            this.buttonStart.Size = new System.Drawing.Size(110, 38);
+            this.buttonStart.Size = new System.Drawing.Size(73, 25);
             this.buttonStart.TabIndex = 5;
             this.buttonStart.Text = "Start";
             this.buttonStart.UseVisualStyleBackColor = true;
@@ -106,64 +94,72 @@
             // 
             // buttonStop
             // 
-            this.buttonStop.Location = new System.Drawing.Point(181, 460);
+            this.buttonStop.Location = new System.Drawing.Point(206, 86);
+            this.buttonStop.Margin = new System.Windows.Forms.Padding(2);
             this.buttonStop.Name = "buttonStop";
-            this.buttonStop.Size = new System.Drawing.Size(95, 38);
+            this.buttonStop.Size = new System.Drawing.Size(63, 25);
             this.buttonStop.TabIndex = 6;
             this.buttonStop.Text = "Stop";
             this.buttonStop.UseVisualStyleBackColor = true;
             this.buttonStop.Click += new System.EventHandler(this.ButtonStop_Click);
             // 
-            // timer1
+            // timer
             // 
-            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            this.timer.Tick += new System.EventHandler(this.Timer1_Tick);
             // 
             // labelTicks
             // 
             this.labelTicks.AutoSize = true;
-            this.labelTicks.Location = new System.Drawing.Point(18, 427);
+            this.labelTicks.Location = new System.Drawing.Point(10, 65);
+            this.labelTicks.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelTicks.Name = "labelTicks";
-            this.labelTicks.Size = new System.Drawing.Size(41, 20);
+            this.labelTicks.Size = new System.Drawing.Size(30, 13);
             this.labelTicks.TabIndex = 7;
-            this.labelTicks.Text = "ticks";
+            this.labelTicks.Text = "Tiks:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(270, 427);
+            this.label2.Location = new System.Drawing.Point(178, 65);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(57, 20);
+            this.label2.Size = new System.Drawing.Size(71, 13);
             this.label2.TabIndex = 8;
-            this.label2.Text = "Motion";
+            this.label2.Text = "Motion Index:";
             // 
             // buttonVideo
             // 
-            this.buttonVideo.Location = new System.Drawing.Point(298, 460);
+            this.buttonVideo.Location = new System.Drawing.Point(284, 86);
+            this.buttonVideo.Margin = new System.Windows.Forms.Padding(2);
             this.buttonVideo.Name = "buttonVideo";
-            this.buttonVideo.Size = new System.Drawing.Size(81, 38);
+            this.buttonVideo.Size = new System.Drawing.Size(54, 25);
             this.buttonVideo.TabIndex = 9;
             this.buttonVideo.Text = "Video";
             this.buttonVideo.UseVisualStyleBackColor = true;
             this.buttonVideo.Click += new System.EventHandler(this.ButtonVideo_Click);
             // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(410, 609);
+            this.ClientSize = new System.Drawing.Size(352, 493);
             this.Controls.Add(this.buttonVideo);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.labelTicks);
             this.Controls.Add(this.buttonStop);
             this.Controls.Add(this.buttonStart);
             this.Controls.Add(this.takePictureBtn);
-            this.Controls.Add(this.labelSaved);
             this.Controls.Add(this.videoSourcePlayer);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.devicesCombo);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
-            this.Text = "Capture";
+            this.Text = "Webcam Spy";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -175,14 +171,14 @@
         private System.Windows.Forms.ComboBox devicesCombo;
         private System.Windows.Forms.Label label1;
         private AForge.Controls.VideoSourcePlayer videoSourcePlayer;
-        private System.Windows.Forms.Label labelSaved;
         private System.Windows.Forms.Button takePictureBtn;
         private System.Windows.Forms.Button buttonStart;
         private System.Windows.Forms.Button buttonStop;
-        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer;
         private System.Windows.Forms.Label labelTicks;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button buttonVideo;
+        private System.Windows.Forms.ImageList imageList1;
     }
 }
 
--- Inscription/Form1.cs
+++ Inscription/Form1.cs
@@ -4,81 +4,71 @@
  * @Description: Capture Images from WebCam
  */
 
+using Accord.Video.FFMPEG;
+using AForge.Video.DirectShow;
+using AForge.Vision.Motion;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-using AForge.Video.DirectShow;
-using System.Drawing.Imaging;
-using System.Net;
-using System.IO;
-using AForge.Vision.Motion;
-
-
+//
+//From https://github.com/Reddine/Webcam-Capture-AForge.Net
+//
+//
 namespace Inscription
 {
     public partial class Form1 : Form
     {
-        //
-        //From https://github.com/Reddine/Webcam-Capture-AForge.Net
-        //
-        //
-        FilterInfoCollection videoDevices;
-        private MotionDetector detector = new MotionDetector((IMotionDetector)new TwoFramesDifferenceDetector(), (IMotionProcessing)null);
-        List<Bitmap> currentFrames = new List<Bitmap>();
-        List<float> MotionLevels = new List<float>();
-        VideoCaptureDevice device;
-        List<string> JpegFileNames = new List<string>();
+        private long tickCounter = 0;
+        private const string format = "yyyy_MM_dd_HH_mm_ss_fff";
 
-        long tickCounter = 0;
+        private FilterInfoCollection videoDevices;
+        private readonly MotionDetector detector = new MotionDetector(new TwoFramesDifferenceDetector(), null);
+        private readonly List<Bitmap> currentFrames = new List<Bitmap>();
+        private readonly List<float> motionLevels = new List<float>();
+
         public Form1()
         {
             InitializeComponent();
 
-            // show device list
             try
             {
-                // enumerate video devices
+                /// List of webcams
                 videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
 
                 if (videoDevices.Count == 0)
                     throw new ApplicationException();
 
-                // add all devices to combo
+                /// Fill list of devices
                 foreach (FilterInfo device in videoDevices)
-                {
                     devicesCombo.Items.Add(device.Name);
-                }
             }
-            catch (ApplicationException)
+            catch (Exception)
             {
                 devicesCombo.Items.Add("No local capture devices");
                 devicesCombo.Enabled = false;
                 takePictureBtn.Enabled = false;
             }
-
-            devicesCombo.SelectedIndex = 0;
-
-            
+            finally
+            {
+                devicesCombo.SelectedIndex = 0;
+            }
         }
 
         private void takePictureBtn_Click(object sender, EventArgs e)
         {
-            DateTime time = DateTime.Now;              // Use current time
-	        string format = "yyyy_MM_dd_HH_mm_ss";    // Use this format
-            String strFilename = "Capture-" + time.ToString(format) + ".jpg";
+            DateTime time = DateTime.Now;
+            string format = "yyyy_MM_dd_HH_mm_ss";
+            string strFilename = "Capture-" + time.ToString(format) + ".jpg";
             if (videoSourcePlayer.IsRunning)
             {
                 Bitmap picture = videoSourcePlayer.GetCurrentVideoFrame();
                 picture.Save(strFilename, ImageFormat.Jpeg);
                 currentFrames.Add(picture);
-                labelSaved.Text = "Capture Saved : " + strFilename;                
+                MessageBox.Show($"Кадр сохранён: {strFilename}", "Оповещение");
             }
         }
 
@@ -92,155 +82,116 @@ namespace Inscription
 
         private void devicesCombo_SelectedIndexChanged(object sender, EventArgs e)
         {
-            //videoSourcePlayer.SignalToStop();
-            //videoSourcePlayer.WaitForStop();
-            //VideoCaptureDevice videoCaptureSource = new VideoCaptureDevice(videoDevices[devicesCombo.SelectedIndex].MonikerString);
-            //videoSourcePlayer.VideoSource = videoCaptureSource;
-            //videoSourcePlayer.Start();
+            if (tickCounter != 0)            
+                RestartStreamCam();           
         }
+
+        #region Start/Stop/Restart camera
+        private void RestartStreamCam()
+        {
+            StopStreamCam();
+            StartStreamCam();
+        }
+
+        private void StopStreamCam()
+        {
+            videoSourcePlayer.SignalToStop();
+            videoSourcePlayer.WaitForStop();
+        }
+
+        private void StartStreamCam()
+        {
+            videoSourcePlayer.VideoSource = new VideoCaptureDevice(videoDevices[devicesCombo.SelectedIndex].MonikerString);
+            videoSourcePlayer.Start();
+        }
+        #endregion
 
         private void ButtonStart_Click(object sender, EventArgs e)
         {
-            VideoCaptureDevice videoCaptureSource = new VideoCaptureDevice(videoDevices[devicesCombo.SelectedIndex].MonikerString);
-            device = videoCaptureSource;    
-            videoSourcePlayer.VideoSource = videoCaptureSource;
-            videoSourcePlayer.Start();
-            timer1.Start();
+            RestartStreamCam();
+            timer.Start();
         }
 
         private void ButtonStop_Click(object sender, EventArgs e)
         {
-            videoSourcePlayer.SignalToStop();
-            videoSourcePlayer.WaitForStop();
-            timer1.Stop();
+            StopStreamCam();
+            timer.Stop();
         }
+
         private void CreateVideo()
         {
-            int width = 640;
-            int height = 480;
-            Image f = Image.FromFile(JpegFileNames[0]);
-            width = f.Width;
-            height = f.Height;
+            Image fFrame = currentFrames.First();
+            Size size = new Size(fFrame.Width, fFrame.Height);
 
-            Accord.Video.FFMPEG.VideoFileWriter vw = new Accord.Video.FFMPEG.VideoFileWriter();
-            
-            // create instance of video writer
-            //VideoFileWriter writer = new VideoFileWriter();
-            // create new video file
-            string format = "yyyy_MM_dd_HH_mm_ss_fff";    // Use this format
-            DateTime time = DateTime.Now;
-            String strFilename = "Video-" + time.ToString(format) + ".avi";
-            vw.Open(strFilename, width, height, 2, Accord.Video.FFMPEG.VideoCodec.MPEG4);
-            //writer.Open(strFilename, width, height, 25, VideoCodec.MPEG4);
-            // create a bitmap to save into the video file
-            Bitmap image = new Bitmap(width, height, PixelFormat.Format24bppRgb);
-            // write video frames
-            foreach (var file in JpegFileNames)
+            using (VideoFileWriter vw = new VideoFileWriter())
             {
-                Bitmap img = (Bitmap)Bitmap.FromFile(file);
-                vw.WriteVideoFrame(img);
+                string strFilename = $"Video_{DateTime.Now.ToString(format)}.avi";
+                vw.Open(strFilename, size.Width, size.Height, 10, VideoCodec.MPEG4);
+
+                /// Create video frames
+                foreach (var img in currentFrames)
+                    vw.WriteVideoFrame(img);
+
+                vw.Close();
             }
-            vw.Close();
-            JpegFileNames.Clear();
         }
         private void Timer1_Tick(object sender, EventArgs e)
         {
-            if (tickCounter>0 && tickCounter % 600 == 0)
+            if (tickCounter > 0 && tickCounter % 600 == 0)
             {
-                //count average motion over minute
-                float level = AverageMotion(60);//porog 0.07
+                /// Avg motion on 1 minutes
+                float index = AverageMotion(60);
                 float max = MaxMotion(60);
-                //MessageBox.Show("level=" +level+" max="+max) ;
-                label2.Text = "level=" + level + " max=" + max;
-                if (level > 0.07)
-                {
-                    //save jpegs to avi
+                label2.Text = $"index:{Math.Round(index, 5)} max: {Math.Round(max, 5)}";
+                if (index > 0.07)
                     CreateVideo();
-                }
-                else
-                {
-                    //delete files
-                    foreach (var file in JpegFileNames)
-                    {
-                        try
-                        {
-                            File.Delete(file);
-                        }
-                        catch (Exception)
-                        {
-                            //ничего не делаем                            
-                        }
-                    }
-                    JpegFileNames.Clear();
-                }
             }
-            labelTicks.Text = tickCounter.ToString();
+            
             if (videoSourcePlayer.IsRunning)
             {
+                /// Take a picture 1 time in half a second
                 Bitmap picture = videoSourcePlayer.GetCurrentVideoFrame();
-                if (tickCounter % 5 == 0 && picture != null)
+                if (picture != null)
                 {
-                    string format = "yyyy_MM_dd_HH_mm_ss_fff";    // Use this format
-                    DateTime time = DateTime.Now;
-                    String strFilename = "Capture-" + time.ToString(format) + ".jpg";
-                    picture.Save(strFilename);
-                    JpegFileNames.Add(strFilename);
-                }
-                //currentFrames.Add(picture);
-                //takePictureBtn_Click(null, null);
-                //picture = currentFrames.Last();
-                if (tickCounter % 10 == 0 && picture != null)
-                {
-                    float num = detector.ProcessFrame(picture);
-                    MotionLevels.Add(num);
+                    float diff = detector.ProcessFrame(picture);
+                    currentFrames.Add(picture);
+                    motionLevels.Add(diff);
                 }
             }
-            tickCounter++;
+
+            labelTicks.Text = $"{tickCounter++}";
         }
+
         public float AverageMotion(int seconds)
         {
-            try
-            {
-                float num1 = 0.0f;
-                int num2 = MotionLevels.Count - 1 - seconds;
-                if (num2 < 0) num2 = 0;
-                for (int index = MotionLevels.Count - 1; index > num2; --index)
-                    num1 += MotionLevels[index];
-                return num1 / (float)seconds;
-            }
-            catch
-            {
-                return 0.0f;
-            }
+            float num1 = 0.0f;
+            int num2 = motionLevels.Count - 1 - seconds;
 
+            if (num2 < 0)
+                num2 = 0;
+
+            for (int index = motionLevels.Count - 1; index > num2; --index)
+                num1 += motionLevels[index];
+
+            return num1 / seconds;
         }
+
         public float MaxMotion(int seconds)
         {
-            try
-            {
-                float max = -10;
-                int num2 = MotionLevels.Count - 1 - seconds;
-                if (num2 < 0) num2 = 0;
-                for (int index = MotionLevels.Count - 1; index > num2; --index)
-                    if (MotionLevels[index] > max)
-                        max = MotionLevels[index];
-                return max;
-            }
-            catch
-            {
-                return 0.0f;
-            }
+            float max = -10;
+            int num2 = motionLevels.Count - 1 - seconds;
+            if (num2 < 0)
+                num2 = 0;
 
+            for (int index = motionLevels.Count - 1; index > num2; --index)
+                if (motionLevels[index] > max)
+                    max = motionLevels[index];
+
+            return max;
         }
 
         private void ButtonVideo_Click(object sender, EventArgs e)
         {
-            string dir = Directory.GetCurrentDirectory();
-            var files = Directory.GetFiles(dir, "*.jpg");
-            foreach (var f in files)
-            {
-                JpegFileNames.Add(f);
-            }
             CreateVideo();
         }
     }
